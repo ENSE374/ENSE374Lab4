2ca0765e7e839d47daf7a2077c88c7f13df62623
Hao <zsh9519@gmail.com>
2016-11-14 21:59:55 -0600

    Add rabbit class

--- main/src/fox.java
+++ main/src/fox.java
@@ -3,7 +3,7 @@
 public class fox extends Meat{
 	private String sounds;
 	
-	public fox(int hungry1,int speed1,boolean death1,String name1,String sounds1)
+	public fox(String name1,int hungry1,int speed1,boolean death1,String sounds1)
 	{
 		super(hungry1, speed1, death1,name1);
 		sounds=sounds1;
--- main/src/main.java
+++ main/src/main.java
@@ -7,10 +7,11 @@ public class main{
 		System.out.println("------------------------");
 		
 		wolf w=new wolf("wolf",3,3,false,"Wooooo");
-		fox f=new fox(2,2,false,"fox","Fooooooooo");
-		hawk h=new hawk("hawk",3,3,false,"Gaaaaaaaaa");
+		fox f=new fox("fox",2,2,false,"Fooooooooo");
+		hawk h=new hawk("hawk",3,4,false,"Gaaaaaaaaa");
 		
 		deer d=new deer("Deer",3,3,false,"Deeeeeee");
+		rabbit r=new rabbit("rabbit",3,2,false,"raaaaaaaaa");
 		grass g=new grass(false,"Grass");
 		tree t=new tree(false,"Tree");
 
