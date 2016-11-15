ecde639f489cb2471c1ed66d30776c16312c00cd
Hao <zsh9519@gmail.com>
2016-11-14 21:49:59 -0600

    Add the new Classes and connect with different animal types

--- main/src/main.java
+++ main/src/main.java
@@ -7,9 +7,11 @@ public class main{
 		
 		wolf w=new wolf("wolf",3,3,false,"Wooooo");
 		fox f=new fox(2,2,false,"fox","jajajajajaja");
+		hawk h=new hawk("hawk",3,3,false,"Gaaaaaaaaa");
 		grass g=new grass(false,"Grass");
 		tree t=new tree(false,"Tree");
 
+		h.print();
 		f.print();
 		w.print();
 		t.print();
