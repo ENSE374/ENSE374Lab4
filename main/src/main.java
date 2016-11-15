124395307fbee59d650dd8d0b9d2f727da276dd1
Hao <zsh9519@gmail.com>
2016-11-14 21:55:53 -0600

    Add the deer class and override the eat function

--- main/src/main.java
+++ main/src/main.java
@@ -4,13 +4,17 @@ public class main{
 	public static void main (String[] args) {
 		
 		System.out.println("Saskatchewan prairies........");
+		System.out.println("------------------------");
 		
 		wolf w=new wolf("wolf",3,3,false,"Wooooo");
-		fox f=new fox(2,2,false,"fox","jajajajajaja");
+		fox f=new fox(2,2,false,"fox","Fooooooooo");
 		hawk h=new hawk("hawk",3,3,false,"Gaaaaaaaaa");
+		
+		deer d=new deer("Deer",3,3,false,"Deeeeeee");
 		grass g=new grass(false,"Grass");
 		tree t=new tree(false,"Tree");
 
+		d.eat(g);
 		h.print();
 		f.print();
 		w.print();
