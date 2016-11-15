bdf8b7750d323ea97191930613961ba48610938f
Hao <zsh9519@gmail.com>
2016-11-14 21:20:14 -0600

    Add the original plant class as the parent class, and the grass class will inherent the plant class.

--- main/src/main.java
+++ main/src/main.java
@@ -1,13 +1,17 @@
 
-
 public class main{
 
 	public static void main (String[] args) {
+		
+		System.out.println("Saskatchewan prairies........");
+		
 		wolf w=new wolf("wolf",3,3,false,"Wooooo");
 		fox f=new fox(2,2,false,"fox","cccc");
-		w.eat(f);
-		w.eat(f);
+		grass g=new grass(false,"Grass");
+
 		w.print3();
+		
+		g.print();
 	}
 
 }
